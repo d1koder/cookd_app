d60d14a2a3d8a032e7f1d47e49fe8a5aa26090a8
DK <dcsk.daniel@gmail.com>
2020-11-27 13:55:48 +1100

    created chefs table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_27_020232) do
+ActiveRecord::Schema.define(version: 2020_11_27_025138) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "chefs", force: :cascade do |t|
+    t.bigint "profile_id", null: false
+    t.float "year_experience"
+    t.float "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["profile_id"], name: "index_chefs_on_profile_id"
+  end
 
   create_table "cuisines", force: :cascade do |t|
     t.string "country"
@@ -44,5 +53,6 @@ ActiveRecord::Schema.define(version: 2020_11_27_020232) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "chefs", "profiles"
   add_foreign_key "profiles", "users"
 end
