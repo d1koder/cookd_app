65a716322be637cb25dc4534ec9cf8bfefd90d28
DK <dcsk.daniel@gmail.com>
2020-12-02 10:14:55 +1100

    made comments to controllers, added required:true

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,14 +1,17 @@
 class ChefsController < ApplicationController
     before_action :set_profile, only: [ :create ]
 
+    #Get all chefs from database
     def index
         @chefs = Chef.all
     end
 
+    #New chef object 
     def new
         @chef = Chef.new
     end
 
+    #Create new chef in database
     def create
         @chef = Chef.new(chef_params)
         @chef.profile_id = @profile.id
@@ -34,10 +37,12 @@ class ChefsController < ApplicationController
 
     private
 
+    #strong params for chef
     def chef_params
         params.require(:chef).permit(:year_experience, :price)
     end
-
+    
+    #set profile for chef
     def set_profile
       @profile = Profile.find_by(user_id: current_user.id)
     end
